df52bcc43948267008e2e37ca78569c6965d286b
Emile Butzbach <emile.butzbach@epipactis.polytechnique.fr>
2024-11-01 12:25:27 +0100

    Changing size of transformer

--- eeg2vec/models/cnn_encoder.py
+++ eeg2vec/models/cnn_encoder.py
@@ -4,10 +4,10 @@ class CNNEncoder(nn.Module):
     def __init__(self):
         super(CNNEncoder, self).__init__()
         self.conv_layers = nn.Sequential(
-            nn.Conv1d(in_channels=5, out_channels=32, kernel_size=5, stride=1),
+            nn.Conv1d(in_channels=5, out_channels=16, kernel_size=2, stride=1),
             nn.ReLU(),
             nn.MaxPool1d(kernel_size=2),
-            nn.Conv1d(in_channels=32, out_channels=64, kernel_size=5, stride=1),
+            nn.Conv1d(in_channels=16, out_channels=16, kernel_size=2, stride=1),
             nn.ReLU()
         )
     def forward(self, x):
