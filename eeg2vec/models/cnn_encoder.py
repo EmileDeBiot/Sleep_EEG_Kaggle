594f41807e74ee105f1f8ea6d1422dc315d2abd6
EmileDeBiot <115916424+EmileDeBiot@users.noreply.github.com>
2024-11-12 16:49:23 +0100

    :sparkles: Add pretraining with sleep data and smaller models

--- eeg2vec/models/cnn_encoder.py
+++ eeg2vec/models/cnn_encoder.py
@@ -4,10 +4,10 @@ class CNNEncoder(nn.Module):
     def __init__(self):
         super(CNNEncoder, self).__init__()
         self.conv_layers = nn.Sequential(
-            nn.Conv1d(in_channels=5, out_channels=16, kernel_size=2, stride=1),
+            nn.Conv1d(in_channels=5, out_channels=8, kernel_size=1, stride=1),
             nn.ReLU(),
-            nn.MaxPool1d(kernel_size=2),
-            nn.Conv1d(in_channels=16, out_channels=16, kernel_size=2, stride=1),
+            nn.MaxPool1d(kernel_size=5),
+            nn.Conv1d(in_channels=8, out_channels=8, kernel_size=2, stride=1),
             nn.ReLU()
         )
     def forward(self, x):
